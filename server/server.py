62fcaf6f5af294650536ef663c2fd00f5b6ac91b
Sankalp Jain <sankalp.jain@somaiya.edu>
2021-02-23 18:06:16 +0530

    change

--- server/server.py
+++ server/server.py
@@ -14,6 +14,7 @@ def classify_image():
 
     return response
 
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
     util.load_saved_artifacts()
--- server/util.py
+++ server/util.py
@@ -10,8 +10,8 @@ __class_number_to_name = {}
 
 __model = None
 
-def classify_image(image_base64_data, file_path=None):
 
+def classify_image(image_base64_data, file_path=None):
     imgs = get_cropped_image_if_2_eyes(file_path, image_base64_data)
 
     result = []
@@ -21,28 +21,30 @@ def classify_image(image_base64_data, file_path=None):
         scalled_img_har = cv2.resize(img_har, (32, 32))
         combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_img_har.reshape(32 * 32, 1)))
 
-        len_image_array = 32*32*3 + 32*32
+        len_image_array = 32 * 32 * 3 + 32 * 32
 
-        final = combined_img.reshape(1,len_image_array).astype(float)
+        final = combined_img.reshape(1, len_image_array).astype(float)
         result.append({
             'class': class_number_to_name(__model.predict(final)[0]),
-            'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_probability': np.around(__model.predict_proba(final) * 100, 2).tolist()[0],
             'class_dictionary': __class_name_to_number
         })
 
     return result
 
+
 def class_number_to_name(class_num):
     return __class_number_to_name[class_num]
+
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
     global __class_name_to_number
     global __class_number_to_name
 
-    with open("./artifacts/class_dictionary.json", "r") as f:
+    with open("/artifacts/class_dictionary.json", "r") as f:
         __class_name_to_number = json.load(f)
-        __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
+        __class_number_to_name = {v: k for k, v in __class_name_to_number.items()}
 
     global __model
     if __model is None:
@@ -62,6 +64,7 @@ def get_cv2_image_from_base64_string(b64str):
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
+
 def get_cropped_image_if_2_eyes(image_path, image_base64_data):
     face_cascade = cv2.CascadeClassifier('./opencv/haarcascades/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('./opencv/haarcascades/haarcascade_eye.xml')
@@ -75,24 +78,26 @@ def get_cropped_image_if_2_eyes(image_path, image_base64_data):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     cropped_faces = []
-    for (x,y,w,h) in faces:
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            if len(eyes) >= 2:
-                cropped_faces.append(roi_color)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if len(eyes) >= 2:
+            cropped_faces.append(roi_color)
     return cropped_faces
+
 
 def get_b64_test_image_for_virat():
     with open("b64.txt") as f:
         return f.read()
 
+
 if __name__ == '__main__':
     load_saved_artifacts()
 
-    print(classify_image(get_b64_test_image_for_virat(), None))
+    # print(classify_image(get_b64_test_image_for_virat(), None))
 
-    # print(classify_image(None, "./test_images/federer1.jpg"))
+    print(classify_image(None, "./test_images/Sharukh_1.jpg"))
     # print(classify_image(None, "./test_images/federer2.jpg"))
     # print(classify_image(None, "./test_images/virat1.jpg"))
     # print(classify_image(None, "./test_images/virat2.jpg"))
